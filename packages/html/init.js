866f0ee2ea62c383a4d006587ff616304d8b071c
songhlc <songhlc@yonyou.com>
2020-01-19 15:29:36 +0800

    添加默认规则

--- packages/html/init.js
+++ packages/html/init.js
@@ -2,6 +2,7 @@ import ast2string from './ast2string'
 import * as html from 'html5parser'
 import * as recast from "recast"
 import { needtraslate } from '../utils'
+import { scriptrules, tagrules, textrules } from './rules'
 var isInScript = false
 /**
  * 
@@ -22,6 +23,11 @@ function init (input, options) {
         if (node.name == "script") {
           isInScript = true
         }
+        // 默认规则
+        tagrules.forEach(rule => {
+          rule(node)
+        })
+        // 自定义规则
         tagRule && tagRule.forEach(rule => {
           rule(node)
         })
@@ -29,6 +35,11 @@ function init (input, options) {
         // html中解析内部js
         if (isInScript) {
           const jsast = recast.parse(node.value);
+          // 默认规则
+          scriptrules.forEach(rule => {
+            rule(jsast)
+          })
+          // 自定义规则
           scriptRule && scriptRule.forEach(rule => {
             rule(jsast)
           })
@@ -37,6 +48,10 @@ function init (input, options) {
           node.value = output
         }
         if (needtraslate(node?.value)) {
+          textrules.forEach(rule => {
+            rule(node)
+          })
+          // 自定义规则
           textRule && textRule.forEach(rule => {
             rule(node)
           })

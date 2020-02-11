f79f57d52d8188ec6087dee8b3428f84c067bc15
zhaohft <zhaohft@yonyou.com>
2020-02-11 21:51:22 +0800

    KO注解代码规范修改

--- packages/rules/html/rule-ko-text.js
+++ packages/rules/html/rule-ko-text.js
@@ -1,11 +1,11 @@
 import { getResourceId, TRANSLATE_METHOD,koAttrReast } from '../../utils'
 const rule = (node) => {
-    if(node.whichTag === "ko"){
-        node.value = node.value.replace(/[\s]*ko[\s]+/,"")
-        node.value = koAttrReast(node)
-        node.value = " ko " + node.value
-    }else if(node.whichTag === "comment" ){
-        //  不处理
+    if(node._isComment){
+        if(/[\s]*ko[\s]+/.test(node.value)){
+            node.value = node.value.replace(/[\s]*ko[\s]+/,"")
+            node.value = koAttrReast(node)
+            node.value = " ko " + node.value
+        }
     }else{
         node.value = `<!-- ko text: ${TRANSLATE_METHOD}('${getResourceId(node.value)}'/*${node.value}*/)--><!-- /ko -->`
     }

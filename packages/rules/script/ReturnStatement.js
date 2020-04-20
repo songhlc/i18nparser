05672eed089ab8b79ccedb0257abaae2aebe7f2c
songhlc <songhlc@yonyou.com>
2020-04-20 22:13:41 +0800

    return statement添加返回值判断

--- packages/rules/script/ReturnStatement.js
+++ packages/rules/script/ReturnStatement.js
@@ -2,7 +2,7 @@ import chooseRule from './chooseRule'
 var rule = (expression) => {
   // expression 分为left 和 right
   if (expression.argument) {
-    chooseRule(expression.argument)
+    chooseRule(expression.argument, expression, 'argument')
   }
 }
 export default rule
--- packages/rules/script/literal.js
+++ packages/rules/script/literal.js
@@ -1,6 +1,7 @@
 import { needtranslate, getResourceId, generateCallExpression, getGlobalData } from '../../utils'
 var rule = (Literal, parentNode, attrKey) => {
   if (needtranslate(Literal.value)) {
+    debugger
     if (parentNode) {
       // 非正则表达式类型才处理
       if (!Literal.regex) {
